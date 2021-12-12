2f7fa40d5469742414c7712419590b60b057bcce
cgusti <cgusti@lion.lmu.edugit config --global user.email cgusti@lion.lmu.edu>
2021-12-12 15:07:12 -0800

    Got firestore authentication to work
    - Only display app contents if user signs in

--- src/App.js
+++ src/App.js
@@ -3,25 +3,32 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Navbar from "./Pages/Navbar";
 import Home from "./Pages/Home";
 import Footer from "./Pages/Footer";
+import { SignIn, SignOut, useAuthentication } from "./authService";
 
 import PortfolioBuilder from "./Pages/PortfolioBuilder";
 
 function App() {
+  const user = useAuthentication();
   return (
     <Router>
       <div className="App">
-        <Navbar />
-        <div className="Content">
-          <Switch>
-            <Route exact path="/">
-              <Home />
-            </Route>
-            <Route path="/PortfolioBuilder">
-              <PortfolioBuilder />
-            </Route>
-          </Switch>
-          <Footer />
-        </div>
+        {!user ? "" : <Navbar />}
+        {!user ? <SignIn /> : <SignOut />}
+        {!user ? (
+          ""
+        ) : (
+          <div className="Content">
+            <Switch>
+              <Route exact path="/">
+                <Home />
+              </Route>
+              <Route path="/PortfolioBuilder">
+                <PortfolioBuilder />
+              </Route>
+            </Switch>
+            <Footer />
+          </div>
+        )}
       </div>
     </Router>
   );
--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -1,9 +1,9 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import {getAuth} from "firebase/auth"
+import {getFirestore} from "firebase/firestore"
 
-// Your web app's Firebase configuration
+
 const firebaseConfig = {
   apiKey: "AIzaSyDa8Hm4MBjyq3t68Rl9rJG6bMFiLo-FoaM",
   authDomain: "freelancerio.firebaseapp.com",
@@ -13,7 +13,7 @@ const firebaseConfig = {
   appId: "1:862365469668:web:81d0b8fe9007865cbe95f1"
 };
 
-// Initialize Firebase
+
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(); 
 export const database = getFirestore();
