3102f2c92aac006124ba5b4bb311a08c212e2f8e
cgusti <cgusti@lion.lmu.edugit config --global user.email cgusti@lion.lmu.edu>
2021-12-14 15:42:08 -0800

    Recreated authService.js

---
+++ src/authService.js
@@ -0,0 +1,31 @@
+import { useState, useEffect } from "react";
+import { signInWithPopup, GoogleAuthProvider, signOut } from "firebase/auth";
+import { auth } from "./firebaseConfig";
+import { FaUserAlt } from "react-icons/fa";
+
+export function SignIn() {
+  return (
+    <button onClick={() => signInWithPopup(auth, new GoogleAuthProvider())}>
+      Sign In
+    </button>
+  );
+}
+
+export function SignOut() {
+  return (
+    <div>
+      <FaUserAlt /> {auth.currentUser.displayName} &nbsp;
+      <button onClick={() => signOut(auth)}>Sign Out</button>
+    </div>
+  );
+}
+
+export function useAuthentication() {
+  const [user, setUser] = useState(null);
+  useEffect(() => {
+    return auth.onAuthStateChanged((user) => {
+      user ? setUser(user) : setUser(null);
+    });
+  }, []);
+  return user;
+}
